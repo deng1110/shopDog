fadbdeac1d2c5f46e73a1c5f516d1d6e8672e52b
deng1110 <647934648@qq.com>
2026-06-02 13:51:25 +0800

    init: shopDog Spring Boot 2.7 skeleton (JDK 8)

---
+++ src/main/java/com/shopdog/ShopDogApplication.java
@@ -0,0 +1,12 @@
+package com.shopdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopDogApplication.class, args);
+    }
+}
