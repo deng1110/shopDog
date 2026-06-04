1551c2d7fb9ceb84bec8299f65da98d833c5a9b7
deng1110 <647934648@qq.com>
2026-06-04 17:31:48 +0800

    fix: accept text/plain from wechat jscode2session response
    微信 jscode2session 返回 JSON 但 Content-Type 为 text/plain,
    给 RestTemplate 的 Jackson 转换器增加 TEXT_PLAIN 支持,
    否则解析响应报 UnknownContentTypeException

--- src/main/java/com/shopdog/config/RestTemplateConfig.java
+++ src/main/java/com/shopdog/config/RestTemplateConfig.java
@@ -2,7 +2,11 @@ package com.shopdog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 /**
  * 注册一个 RestTemplate Bean,用来向微信服务器发 HTTP 请求。
@@ -12,6 +16,13 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        // 微信接口(如 jscode2session)返回 JSON 内容但 Content-Type 标的是 text/plain,
+        // 默认 Jackson 转换器只认 application/json,这里让它也接受 text/plain,
+        // 否则解析响应时报 UnknownContentTypeException
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN));
+        restTemplate.getMessageConverters().add(0, converter);
+        return restTemplate;
     }
 }
